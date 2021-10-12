01a7014f757136215eec5a34e6e408c208899ddf
Sandaru Ashen <sandaruashen02@gmail.com>
2021-10-12 19:41:34 +0530

    'enhancements'

--- py_fuscate.py
+++ py_fuscate.py
@@ -4,7 +4,8 @@
 
 
 import os
-import pip
+import sys
+import subprocess
 import argparse
 import random
 import time
@@ -20,9 +21,17 @@ try:
     import colorama
     import pyfiglet
 except ModuleNotFoundError:
-    print('\x1b[1m\x1b[31m' + '[!] required dependencies aren\'t installed\ninstalling..'.title())
-    pip.main(['install', 'pyfiglet', 'colorama', 'tqdm', 'requests'])
-    exit('\x1b[1m\x1b[92m' + '[+] dependencies installed\nrun the program again'.title())
+    if os.name == 'nt':
+        tmp = 'python'
+    else:
+        tmp = 'python' + '.'.join(str(i) for i in sys.version_info[:2])
+    if subprocess.run([tmp, '-m', 'pip', 'install','-r', 'requirements.txt']).returncode == 0:
+        sys.exit('\x1b[1m\x1b[92m' + '[+] dependencies installed\nrun the program again'.title().center(os.get_terminal_size().columns))
+    elif subprocess.run(['pip3', 'install','-r', 'requirements.txt']).returncode == 0:
+        sys.exit('\x1b[1m\x1b[92m' + '[+] dependencies installed\nrun the program again'.title().center(os.get_terminal_size().columns))
+    else:
+        sys.exit('\x1b[1m\x1b[31m' + '[!] something error occured while installing dependencies\n maybe pip isn\'t installed or requirements.txt file not available?'.title().center(os.get_terminal_size().columns))
+PY_VER = 'python' + '.'.join(str(i) for i in sys.version_info[:2])
 BLU = colorama.Style.BRIGHT + colorama.Fore.BLUE
 CYA = colorama.Style.BRIGHT + colorama.Fore.CYAN
 GRE = colorama.Style.BRIGHT + colorama.Fore.GREEN
@@ -43,11 +52,11 @@ colorama.init(autoreset=True)
 def marsh_enc(**kwargs):
     '''argumets:
     source <- source code of python program that you want to obfuscate as a string'''
-    if not(isinstance(kwargs['source'],str)):
+    if not isinstance(kwargs['source'], str):
         print('argumet source must be string'.title())
         return None
     sel_cyph = random.choice((lzma, gzip, bz2, binascii, zlib))
-    source_marshal = marshal.dumps(compile(kwargs['source'],'Py-Fuscate','exec'))
+    source_marshal = marshal.dumps(compile(kwargs['source'], 'Py-Fuscate', 'exec'))
     if sel_cyph is binascii:
         cyph_compressed = binascii.b2a_base64(source_marshal)
     else:
@@ -71,9 +80,9 @@ def logo():
 def parse_args():
     parser = argparse.ArgumentParser(epilog='example: python3 py_fuscate.py -i myprogram.py -o pyprogram_encoded.py -s 100',description='obfuscate python programs'.title())
     parser._optionals.title = "syntax".title()
-    parser.add_argument('-i','--input',type=str,help='input file name'.title(), required=True)
-    parser.add_argument('-o','--output',type=str,help='output file name'.title(), required=True)
-    parser.add_argument('-s','--strong',type=int,help='strengthness of obfuscation'.title(), required=True)
+    parser.add_argument('-i', '--input', type=str, help='input file name'.title(), required=True)
+    parser.add_argument('-o', '--output', type=str, help='output file name'.title(), required=True)
+    parser.add_argument('-s', '--strong', type=int, help='strengthness of obfuscation. 100 recomended'.title(), required=True)
     return parser.parse_args()
 
 def check_update():
@@ -87,21 +96,21 @@ def check_update():
 
 def update(latest_ver):
     if '.git' in os.listdir():
-        os.system('git stash && git pull')
+        process = subprocess.run(['git', 'stash', '&&', 'git', 'pull'])
     else:
         latest_source = requests.get('https://raw.githubusercontent.com/Sl-Sanda-Ru/Py-Fuscate/main/py_fuscate.py').content
-        with open('py_fuscate.py','wb') as file:
+        with open('py_fuscate.py', 'wb') as file:
             file.write(latest_source)
-        with open('.version','w') as file:
+        with open('.version', 'w') as file:
             file.write(latest_ver)
 
 def main():
     args = parse_args()
     if check_update():
-        print(colorama.Style.BRIGHT + colorama.Fore.RED + '\t[!] update available'.title())
-        print(colorama.Style.BRIGHT + colorama.Fore.LIGHTGREEN_EX + '\t[+] updating...'.title())
+        print(colorama.Style.BRIGHT + colorama.Fore.RED + '\t[!] update available'.title().center(os.get_terminal_size().columns))
+        print(colorama.Style.BRIGHT + colorama.Fore.LIGHTGREEN_EX + '\t[+] updating...'.title().center(os.get_terminal_size().columns))
         update(latest_ver)
-        exit(colorama.Style.BRIGHT + colorama.Fore.LIGHTGREEN_EX + '\t[+] successfully updated...\n\t run the program again'.title())
+        sys.exit(colorama.Style.BRIGHT + colorama.Fore.LIGHTGREEN_EX + '\t[+] successfully updated...\n\t run the program again'.title().center(os.get_terminal_size().columns))
     print(random.choice(COLORS) + '\t[+] encoding '.title() + args.input)
     encoded_pro = ''
     with tqdm.tqdm(total=args.strong) as pbar:
@@ -112,8 +121,8 @@ def main():
                 encoded_pro = marsh_enc(source=marsh_enc(source=encoded_pro))
             time.sleep(0.1)
             pbar.update(1)
-    with open(args.output,'w') as file:
-        file.write(f'#Encoded By Py-Fuscate\n#https://github.com/Sl-Sanda-Ru/Py-Fuscate\ntry:\n\t{encoded_pro}\nexcept KeyboardInterrupt:\n\tpass')
+    with open(args.output, 'w') as file:
+        file.write(f'#Encoded By Py-Fuscate\n#https://github.com/Sl-Sanda-Ru/Py-Fuscate\nMake Sure You\'re Running The Program With {PY_VER} Otherwise It Will Crash\nTo Check Your Python Version Run "python -V" Command\ntry:\n\t{encoded_pro}\nexcept KeyboardInterrupt:\n\tpass')
     print(colorama.Style.BRIGHT+colorama.Fore.LIGHTGREEN_EX + '\t[+] encoding successful!\n\tsaved as '.title() + args.output)
 if __name__ == '__main__':
     logo()
